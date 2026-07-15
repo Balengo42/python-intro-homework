e762cfc0991e7a6e20b6ad0e9c3475c0fe69cd96
Kevin <kr71191@gmail.com>
2026-07-14 23:55:12 -0400

    Add warmup3 for week 2

---
+++ week-2/assignment-2/warmup3.py
@@ -0,0 +1,4 @@
+# git log --oneline output:
+# 867de6d Add warmup1 and warmup2 for week 2
+
+print("This week I learned how painful it was to setup everything, but thankfully after having done it I am now comfortable creating and switching Git branches, and also navigating folders in Git")
