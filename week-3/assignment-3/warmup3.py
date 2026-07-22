99bda87f843494cf355478536d35ce8df415dae3
Kevin <kr71191@gmail.com>
2026-07-21 21:42:45 -0400

    warmup3.py

---
+++ week-3/assignment-3/warmup3.py
@@ -0,0 +1,14 @@
+print(not True and False) 
+print("# False, because not True is False, and False and False is False", end="\n\n")
+
+print(True or False and False) 
+print("# True, because since we use or, it could either be True or False, in which we are given false so its true", end="\n\n")
+
+print(not(5 >3)) 
+print("#False, because its True that 5 >3, but since we used not, it becomes False", end="\n\n")
+
+print(10 == 10 and 4 != 4)
+print("# False, because 10 == 10 is True, but 4 != 4 is False, so its False", end="\n\n")
+
+print(not False or not True)
+print("# True, because not False is True, and not True is False, so its True", end="\n\n")
