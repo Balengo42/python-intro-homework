13f0cc0d73c1506f8a118a3f567097cb1c7f9af2
Kevin <kr71191@gmail.com>
2026-07-21 23:32:10 -0400

    changes to warmup3.py

--- week-3/assignment-3/warmup3.py
+++ week-3/assignment-3/warmup3.py
@@ -1,14 +1,9 @@
-print(not True and False) 
-print("# False, because not True is False, and False and False is False", end="\n\n")
+print(f"{not True and False}   # not True is False; False and False is False")
 
-print(True or False and False) 
-print("# True, because since we use or, it could either be True or False, in which we are given false so its true", end="\n\n")
+print(f"{True or False and False}   # True, because since we use or, it could either be True or False, in which we are given false so its true")
 
-print(not(5 >3)) 
-print("#False, because its True that 5 >3, but since we used not, it becomes False", end="\n\n")
+print(f"{not (5 > 3)}   # False, because its True that 5 >3, but since we used not, it becomes False")
 
-print(10 == 10 and 4 != 4)
-print("# False, because 10 == 10 is True, but 4 != 4 is False, so its False", end="\n\n")
+print(f"{10 == 10 and 4 != 4}   # False, because 10 == 10 is True, but 4 != 4 is False, so its False")
 
-print(not False or not True)
-print("# True, because not False is True, and not True is False, so its True", end="\n\n")
+print(f"{not False or not True}   # True, because not False is True, and not True is False, so its True")
