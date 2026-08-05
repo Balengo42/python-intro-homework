748fadfd212aab4ec90c00aae283104d2bcb0fb5
Kevin <kr71191@gmail.com>
2026-08-04 23:57:22 -0400

    fixed up formatting so that it fit more with the assignment, suggestion from airhub

--- week-5/assignment-5/warmup2.py
+++ week-5/assignment-5/warmup2.py
@@ -1,4 +1,5 @@
-Ask= input("Please enter a positive integer: ")
+Ask= input("Enter a positive integer: ")
 while not (Ask.isdigit() and int(Ask) > 0):
-    Ask = input("That's not a positive integer. Try again. \n")
+    print("That's not a positive integer. Try again.")
+    Ask = input("Enter a positive integer: ")
 print(f"Got it: {Ask}")
