7f81d7ad486e050c5e4d661d4811c8e382a2a988
Kevin <kr71191@gmail.com>
2026-08-11 23:28:17 -0400

    warmup3.py

---
+++ week-6/assignment-6/warmup3.py
@@ -0,0 +1,14 @@
+def adding(numb):
+    total = numb + 10 
+
+# print (total)
+#NameError: name 'total' is not defined
+
+
+def adding_fix(numb):
+    total = numb + 10
+    return f"This is the total: {total}"
+
+the_total= adding_fix(4)
+
+print(the_total)
