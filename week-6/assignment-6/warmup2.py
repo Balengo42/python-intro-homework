5c9a9cbb97ed00372f30b4161015490f548c5970
Kevin <kr71191@gmail.com>
2026-08-11 23:27:32 -0400

    Chnaged function so instead of printing, it returns. Also changed name to align with assignment, advice given from airhub.

--- week-6/assignment-6/warmup2.py
+++ week-6/assignment-6/warmup2.py
@@ -1,13 +1,11 @@
-def weather_fahrenheit(celsius_temp):
-    f_temp= (celsius_temp * 9/5) +32
-    print (f"{celsius_temp}°C = {f_temp:.1f}°F")
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
 
 
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
 
-def weather_celsius(fahrenheit_temp):
-    c_temp = (fahrenheit_temp -32) * 5/9
-    print (f"{fahrenheit_temp}°F = {c_temp:.1f}°C")
 
-weather_fahrenheit(0)
-weather_fahrenheit(100)
-weather_celsius(72)
+print(f"{0}°C = {celsius_to_fahrenheit(0):.1f}°F")
+print(f"{100}°C = {celsius_to_fahrenheit(100):.1f}°F")
+print(f"{72}°F = {fahrenheit_to_celsius(72):.1f}°C")
