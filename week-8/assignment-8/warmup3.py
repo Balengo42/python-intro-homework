93383e11a0843a2280ccc89eea4964bcd5648941
Kevin <kr71191@gmail.com>
2026-08-25 22:43:41 -0400

    changed text to align with airhub advice

--- week-8/assignment-8/warmup3.py
+++ week-8/assignment-8/warmup3.py
@@ -1,6 +1,5 @@
 try:
     with open('../data/missing.txt', 'r') as file:
-        for i, line in enumerate(file, start=1):
-            print(f"Line {i}: {line.strip()}")
+        print('missing.txt')
 except FileNotFoundError:
-    print ("Error: 'missing.txt' was not found. Please check the file path and try again.")
+    print ('Error: "missing.txt" was not found. Please check the file path and try again.")
